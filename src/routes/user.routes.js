938c94e0bc79d318b7a3d25efc56ed4b2f576bf6
Nikita <nikitapandey.github@gmail.com>
2025-03-11 16:29:29 +0530

    feat: add user profile update routes including avatar and cover image uploads

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import {logoutUser,loginUser,registerUser,refreshAccessToken} from "../controllers/user.controllers.js";
+import {logoutUser,loginUser,registerUser,refreshAccessToken, updateUserAvatar} from "../controllers/user.controllers.js";
 import {upload} from "../middlewares/multer.middlewares.js"
 import verifyJWT from "../middlewares/auth.middleware.js"
 
@@ -19,5 +19,17 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT,logoutUser)
 
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJWT,changePassword)
+
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+
+router.route("/update-profile").patch(verifyJWT,updateUserDetails)
+
+router.route("/update-avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+
+router.route("/update-cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
 
 export default router
