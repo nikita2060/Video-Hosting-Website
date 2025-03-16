b1a91bc36105226232ba6ef7d2954a0e860c68cd
Nikita <nikitapandey.github@gmail.com>
2025-03-16 13:52:24 +0530

    feat: restructure app.js to include video and subscription routes

--- src/app.js
+++ src/app.js
@@ -1,24 +1,26 @@
-import express from "express"
-import cors from "cors"  //It stands for cross-origin-resource-sharing ,to safely allow or block requests from other websites, preventing unauthorized access to our data.
-import cookieParser from "cookie-parser"
-const app = express()
-
+import express from "express";
+import cors from "cors";//It stands for cross-origin-resource-sharing ,to safely allow or block requests from other websites, preventing unauthorized access to our data.
+import cookieParser from "cookie-parser";
+const app = express();
 
 app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true    
-}))
+    origin: process.env.CORS_ORIGIN,
+    credentials: true    
+}));
 //  configurations for parsing request body using express middlewares
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true, limit:"16kb"})) //parses URL-encoded data (like form submissions). The extended: true option allows for complex objects and arrays to be encoded in the URL
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({extended: true, limit: "16kb"}));
+app.use(express.static("public"));
+app.use(cookieParser());
 
-//routers import
-import router from "./routes/user.routes.js"
+// routers import
+import userRouter from "./routes/user.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
 
-//routes declaration
-app.use("/api/v1/users",router)  //it will be seen as http://localhost:8000/api/v1/users/register
+// routes declaration
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
-
-export {app}
+export { app };
